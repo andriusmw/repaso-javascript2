a94403c3c1e2bc6a68b60a473682b48f96f30c82
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-08 10:20:24 +0100

    functions 2 arrow functions

--- src/index.js
+++ src/index.js
@@ -7,4 +7,17 @@ let nombre = "Tony"
 console.log(saludar(nombre))
 
 
+//------------ arrow functions -------------
 
+
+const saludar2 = (nombre) => {
+    return 'Hola ' + nombre
+} 
+
+console.log(saludar2(nombre))
+
+
+const saludar3 = (nombre) =>  'Hola ' + nombre
+//Si solo devuelve una línea se puede poner así
+
+console.log(saludar3(nombre))
