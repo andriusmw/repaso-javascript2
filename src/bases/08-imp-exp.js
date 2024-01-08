17d05cbaac9071eeca327da7f1bdbbd0c8cc9d55
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-08 14:45:52 +0100

    08 imp-exp 2

--- src/bases/08-imp-exp.js
+++ src/bases/08-imp-exp.js
@@ -1,4 +1,4 @@
-import heroes from './data/heroes'
+import heroes from '../data/heroes'
 
 
 console.log(heroes)
@@ -7,19 +7,32 @@ console.log(heroes)
 //getHeroById (id)
 //Arrow function
 //find
-const getHeroById = (id) => {
+export const getHeroById = (id) => {
     return heroes.find(hero => hero.id === id)
     /* en el find busca un héroe cuyo id comparado con
     el argumento que recibe sea true*/
 }
-console.log(getHeroById(2))
+//console.log(getHeroById(2))
 
 
 //getHeroByOwner ("DC","Marvel")
 //Arrow function
 //filter
 
-const getHeroByOwner= (owner) => {
+export const getHeroByOwner= (owner) => {
     return heroes.filter(hero => hero.owner === owner)
 }
+//console.log(getHeroByOwner('DC'))
+
+
+//--------------------------------------
+
+/* esto iría en el index con la ruta actual 
+
+import {getHeroById, getHeroByOwner} from './bases/08-imp-exp'
+
+
+console.log(getHeroById(2))
 console.log(getHeroByOwner('DC'))
+
+*/
