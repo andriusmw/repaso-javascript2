16a5d335fc7a01bc25ac2b3c50434f5860d35fba
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-09 11:40:53 +0100

    promesas 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hola mundo")
+const miPromesa = () => {
+    return new Promise( resolve => {
+        setTimeout(() => {
+            resolve("Tenemos un valor en la promesa")
+        }, 1000);
+
+    })
+}
+
+
+
+console.log("Inicio")
+
+miPromesa.then(console.log)
+
+console.log("Fin")
